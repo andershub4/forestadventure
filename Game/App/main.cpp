f9890c1c5de7ca743f25a166975047c91eca1558
Anders Wennmo <anders.wennmo@gmail.com>
2020-12-29 20:44:59 +0100

    Add main

---
+++ Game/App/main.cpp
@@ -0,0 +1,14 @@
+/*
+*	Copyright (C) 2020 Anders Wennmo
+*	This file is part of forestadventure which is released under MIT license.
+*	See file LICENSE for full license details.
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
