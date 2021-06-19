5f271dd8d7840dbbd4ed837a5dbf57f30e23f150
Anders Wennmo <anders.wennmo@gmail.com>
2021-06-19 18:27:13 +0200

    Rename application name to not use any space For some reason, the exe installation file was renamed by github when uploaded it. Github replaced space with .

--- Game/src_lib/Version.h
+++ Game/src_lib/Version.h
@@ -7,7 +7,7 @@
 #pragma once
 
 // If this name is updated, update in inno setup file as well 
-#define FA_APP_NAME "FA Pre-alpha"
+#define FA_APP_NAME "FA-Pre-alpha"
 
 // If this version is updated, update in inno setup file as well
 #define FA_APP_VERSION "0.20"
