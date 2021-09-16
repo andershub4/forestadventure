4cfa817d1cfa04d71090cec48415bd440b49498b
Anders Wennmo <anders.wennmo@gmail.com>
2021-09-16 18:47:13 +0200

    Minor update in Camera

--- Game/src_lib/Level/Camera.cpp
+++ Game/src_lib/Level/Camera.cpp
@@ -14,31 +14,30 @@
 
 namespace FA {
 
-Camera::Camera(const sf::Vector2f& position, const sf::Vector2u& renderTargetSize, const sf::Vector2u& mapSize)
-    : position_(position)
-    , mapSize_(mapSize)
+Camera::Camera(const sf::Vector2f& trackingPoint, const sf::Vector2u& viewSize, const sf::Vector2u& mapSize)
+    : trackingPoint_(trackingPoint)
 {
-    auto size_f = static_cast<sf::Vector2f>(renderTargetSize);
+    auto size_f = static_cast<sf::Vector2f>(viewSize);
     view_.setSize(size_f);
-    centerPos_ = size_f / 2.0f;
-    minViewPosition_ = {centerPos_.x, centerPos_.y};
-    maxViewPosition_ = {mapSize_.x - centerPos_.x, mapSize_.y - centerPos_.y};
+    auto centerPos = size_f / 2.0f;
+    minViewPosition_ = {centerPos.x, centerPos.y};
+    maxViewPosition_ = {mapSize.x - centerPos.x, mapSize.y - centerPos.y};
 }
 
 Camera::~Camera() = default;
 
 void Camera::UpdatePosition(sf::RenderTarget& renderTarget)
 {
-    auto viewPosition = CalcViewPosition(position_);
+    auto viewPosition = CalcViewPosition();
     view_.setCenter(viewPosition);
     renderTarget.setView(view_);
 }
 
-sf::Vector2f Camera::CalcViewPosition(const sf::Vector2f& position) const
+sf::Vector2f Camera::CalcViewPosition() const
 {
     sf::Vector2f viewPosition;
-    viewPosition.x = Clamp(position.x, minViewPosition_.x, maxViewPosition_.x);
-    viewPosition.y = Clamp(position.y, minViewPosition_.y, maxViewPosition_.y);
+    viewPosition.x = Clamp(trackingPoint_.x, minViewPosition_.x, maxViewPosition_.x);
+    viewPosition.y = Clamp(trackingPoint_.y, minViewPosition_.y, maxViewPosition_.y);
 
     return viewPosition;
 }
--- Game/src_lib/Level/Camera.h
+++ Game/src_lib/Level/Camera.h
@@ -15,21 +15,19 @@ namespace FA {
 class Camera
 {
 public:
-    Camera(const sf::Vector2f& position, const sf::Vector2u& renderTargetSize, const sf::Vector2u& mapSize);
+    Camera(const sf::Vector2f& trackingPoint, const sf::Vector2u& renderTargetSize, const sf::Vector2u& mapSize);
     ~Camera();
 
     void UpdatePosition(sf::RenderTarget& renderTarget);
 
 private:
-    sf::Vector2u mapSize_{};
     sf::View view_;
-    sf::Vector2f centerPos_;
-    const sf::Vector2f& position_;
+    const sf::Vector2f& trackingPoint_;
     sf::Vector2f minViewPosition_{};
     sf::Vector2f maxViewPosition_{};
 
 private:
-    sf::Vector2f CalcViewPosition(const sf::Vector2f& position) const;
+    sf::Vector2f CalcViewPosition() const;
     float Clamp(float value, float lower, float upper) const;
 };
 
