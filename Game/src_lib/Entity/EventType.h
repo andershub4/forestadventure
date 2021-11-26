274d1f48ffcb13b2e04b96e5f87c6083cfb6264a
Anders Wennmo <anders.wennmo@gmail.com>
2021-11-26 21:44:08 +0100

    Add possibility to print enum

--- Game/src_lib/Entity/EventType.h
+++ Game/src_lib/Entity/EventType.h
@@ -6,11 +6,51 @@
 
 #pragma once
 
+#include <ostream>
+
 namespace FA {
 
 namespace Entity {
 
 enum class EventType { None, Create, Destroy, Init, StartMove, StopMove, Attack, AttackWeapon, Collision };
+
+inline std::ostream& operator<<(std::ostream& os, const EventType& e)
+{
+    std::string str;
+    switch (e) {
+        case EventType::Attack:
+            str = "Attack";
+            break;
+        case EventType::AttackWeapon:
+            str = "AttackWeapon";
+            break;
+        case EventType::Collision:
+            str = "Collision";
+            break;
+        case EventType::Create:
+            str = "Create";
+            break;
+        case EventType::Destroy:
+            str = "Destroy";
+            break;
+        case EventType::Init:
+            str = "Init";
+            break;
+        case EventType::None:
+            str = "None";
+            break;
+        case EventType::StartMove:
+            str = "StartMove";
+            break;
+        case EventType::StopMove:
+            str = "StopMove";
+            break;
+    }
+
+    os << str;
+
+    return os;
+}
 
 }  // namespace Entity
 
--- Game/src_lib/Entity/ModeType.h
+++ Game/src_lib/Entity/ModeType.h
@@ -6,11 +6,42 @@
 
 #pragma once
 
+#include <ostream>
+
 namespace FA {
 
 namespace Entity {
 
 enum class ModeType { None, Uninitialized, Idle, Move, Attack, AttackWeapon };
+
+inline std::ostream& operator<<(std::ostream& os, const ModeType& e)
+{
+    std::string str;
+    switch (e) {
+        case ModeType::Uninitialized:
+            str = "Uninitialized";
+            break;
+        case ModeType::Attack:
+            str = "Attack";
+            break;
+        case ModeType::AttackWeapon:
+            str = "AttackWeapon";
+            break;
+        case ModeType::Idle:
+            str = "Idle";
+            break;
+        case ModeType::Move:
+            str = "Move";
+            break;
+        case ModeType::None:
+            str = "None";
+            break;
+    }
+
+    os << str;
+
+    return os;
+}
 
 }  // namespace Entity
 
