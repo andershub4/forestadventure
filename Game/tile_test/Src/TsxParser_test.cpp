0d3a639de68fe64c9ad448df1a587910295bef2e
Anders Wennmo <anders.wennmo@gmail.com>
2022-08-08 18:40:01 +0200

    Do EXPECT_CALLs more properly

--- Game/tile_test/Src/TsxParser_test.cpp
+++ Game/tile_test/Src/TsxParser_test.cpp
@@ -21,10 +21,10 @@ TEST(TestTsxParser, ParseShouldFailDueToError)
     XMLDocumentMock docMock;
     FA::Tile::TsxParser<XMLDocumentMock, XMLElementMock> p;
 
-    EXPECT_CALL(docMock, LoadFile(_));
+    EXPECT_CALL(docMock, LoadFile(StrEq("test.txt")));
     EXPECT_CALL(docMock, Error()).WillOnce(Return(true));
 
-    EXPECT_EQ(false, (p.Parse("t", &docMock)));
+    EXPECT_EQ(false, (p.Parse("test.txt", &docMock)));
 }
 
 TEST(TestTsxParser, ParseShouldSucceed)
@@ -33,17 +33,22 @@ TEST(TestTsxParser, ParseShouldSucceed)
     XMLElementMock tileSetElementMock;
     FA::Tile::TsxParser<XMLDocumentMock, XMLElementMock> p;
 
-    EXPECT_CALL(docMock, LoadFile(_));
+    EXPECT_CALL(docMock, LoadFile(StrEq("test.txt")));
     EXPECT_CALL(docMock, Error()).WillOnce(Return(false));
     EXPECT_CALL(docMock, FirstChildElement(StrEq("tileset"))).WillOnce(Return(&tileSetElementMock));
 
     EXPECT_CALL(tileSetElementMock, Attribute(StrEq("name"))).WillOnce(Return("a"));
-    EXPECT_CALL(tileSetElementMock, QueryAttribute(_, An<int*>()))
-        .Times(4)
-        .WillRepeatedly(DoAll(SetArgPointee<1>(3), Return(XML_SUCCESS)));
+    EXPECT_CALL(tileSetElementMock, QueryAttribute(StrEq("tilewidth"), An<int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(3), Return(XML_SUCCESS)));
+    EXPECT_CALL(tileSetElementMock, QueryAttribute(StrEq("tileheight"), An<int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(3), Return(XML_SUCCESS)));
+    EXPECT_CALL(tileSetElementMock, QueryAttribute(StrEq("tilecount"), An<int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(3), Return(XML_SUCCESS)));
+    EXPECT_CALL(tileSetElementMock, QueryAttribute(StrEq("columns"), An<int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(3), Return(XML_SUCCESS)));
 
     XMLElementMock tileElementMock;
-    EXPECT_CALL(tileSetElementMock, FirstChildElement(_)).WillOnce(Return(&tileElementMock));
+    EXPECT_CALL(tileSetElementMock, FirstChildElement(StrEq("tile"))).WillOnce(Return(&tileElementMock));
     EXPECT_CALL(tileElementMock, QueryAttribute(StrEq("id"), An<int*>()))
         .WillOnce(DoAll(SetArgPointee<1>(10), Return(XML_SUCCESS)));
     XMLElementMock imageElementMock;
@@ -51,15 +56,16 @@ TEST(TestTsxParser, ParseShouldSucceed)
 
     // ParseImageElement
     EXPECT_CALL(imageElementMock, Attribute(StrEq("source"))).WillOnce(Return("source_name"));
-    EXPECT_CALL(imageElementMock, QueryAttribute(_, An<unsigned int*>()))
-        .Times(2)
-        .WillRepeatedly(DoAll(SetArgPointee<1>(15), Return(XML_SUCCESS)));
+    EXPECT_CALL(imageElementMock, QueryAttribute(StrEq("width"), An<unsigned int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(15), Return(XML_SUCCESS)));
+    EXPECT_CALL(imageElementMock, QueryAttribute(StrEq("height"), An<unsigned int*>()))
+        .WillOnce(DoAll(SetArgPointee<1>(15), Return(XML_SUCCESS)));
 
     EXPECT_CALL(tileElementMock, FirstChildElement(StrEq("animation"))).WillOnce(Return(nullptr));
 
-    EXPECT_CALL(tileElementMock, NextSiblingElement(_)).WillOnce(Return(nullptr));
+    EXPECT_CALL(tileElementMock, NextSiblingElement(StrEq("tile"))).WillOnce(Return(nullptr));
 
-    EXPECT_EQ(true, (p.Parse("t", &docMock)));
+    EXPECT_EQ(true, (p.Parse("test.txt", &docMock)));
 }
 
 }  // namespace Tile
