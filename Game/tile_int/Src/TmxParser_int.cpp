0a63410fff3038ed00e330cbb239c999b7fc5ee0
Anders Wennmo <anders.wennmo@gmail.com>
2023-03-06 17:44:13 +0100

    Split xmlBuf

--- Game/tile_int/Src/TmxParser_int.cpp
+++ Game/tile_int/Src/TmxParser_int.cpp
@@ -21,21 +21,21 @@ namespace Tile {
 class TmxParserInt : public Test
 {
 protected:
-    using ParseResult = ParseResult<tinyxml2::XMLError>;
-
     TmxParserInt()
         : helper_(std::make_shared<ParseHelper<tinyxml2::XMLElement, tinyxml2::XMLError>>())
         , parser_(helper_)
     {}
 
-    const std::string xmlBuffer_ =
-        "<?xml version =\" 1.0 \" encoding=\" UTF - 8 \"?>\
-                                   < map version = \"1.8\" tiledversion = \"1.8.1\" orientation = \"orthogonal\" renderorder =\
-                                       \"right-down\" width = \"100\" height = \"100\" tilewidth = \"16\" tileheight =\
-                                           \"16\" infinite = \"0\" nextlayerid = \"11\" nextobjectid =\
-                                               \"9\" >\
-        <tileset firstgid = \"1\" source = \"tileset.tsx\" /> <tileset firstgid = \"1089\" source = \"simplewater.tsx\" />\
-        </map>";
+    const std::string xmlVersion_ = "<?xml version =\" 1.0 \" encoding=\" UTF - 8 \"?>";
+    const std::string xmlStartMap_ =
+        "<map version = \"1.8\" tiledversion = \"1.8.1\" orientation = \"orthogonal\" renderorder =\"right-down\" "
+        "width = \"100\" height = \"100\" tilewidth = \"16\" tileheight =\"16\" infinite = \"0\" nextlayerid = \"11\" "
+        "nextobjectid =\"9\">";
+    const std::string xmlTileSet_ =
+        "<tileset firstgid = \"1\" source = \"tileset1.tsx\" /> <tileset firstgid = \"1089\" source = "
+        "\"tileset2.tsx\" />";
+    const std::string xmlEndMap_ = "</map>";
+
     tinyxml2::XMLDocument doc_;
     std::shared_ptr<ParseHelper<tinyxml2::XMLElement, tinyxml2::XMLError>> helper_;
     TmxParser<tinyxml2::XMLDocument, tinyxml2::XMLElement, tinyxml2::XMLError> parser_;
@@ -44,7 +44,9 @@ protected:
 TEST_F(TmxParserInt, ParseValidMapShouldSucceed)
 {
     ParsedTmx parsedTmx;
-    parser_.Parse(doc_, xmlBuffer_, parsedTmx);
+    std::stringstream ss;
+    ss << xmlVersion_ << xmlStartMap_ << xmlTileSet_ << xmlEndMap_;
+    parser_.Parse(doc_, ss.str(), parsedTmx);
 
     EXPECT_FALSE(parsedTmx.tileSets_.empty());
 }
