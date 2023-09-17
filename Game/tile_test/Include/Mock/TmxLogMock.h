7c05d098d178d8096dbbd6280710bd55bcdc2bbf
Anders Wennmo <anders.wennmo@gmail.com>
2023-09-17 14:57:45 +0200

    Move logger mock in Tile In order for the link time substitution to work, it is not possible to use inline keyword. So the definitions of the free functions must be in different projects, i.e. the production project and the test project. Further, the tile_int project that is dependent on the logger mock, must include and add the logger mock as an existing file to the project.

--- Game/tile_test/Include/Mock/TmxLogMock.h
+++ Game/tile_test/Include/Mock/TmxLogMock.h
@@ -1,5 +1,5 @@
 /*
- *	Copyright (C) 2022 Anders Wennmo
+ *	Copyright (C) 2023 Anders Wennmo
  *	This file is part of forestadventure which is released under MIT license.
  *	See file LICENSE for full license details.
  */
@@ -15,19 +15,15 @@ namespace Tile {
 class LoggerMock
 {
 public:
-    LoggerMock() { Instance() = this; }
-    ~LoggerMock() { Instance() = nullptr; }
+    LoggerMock() { instance_ = this; }
+    ~LoggerMock() { instance_ = nullptr; }
 
     LoggerMock(const LoggerMock&) = delete;
     LoggerMock& operator=(const LoggerMock&) = delete;
     LoggerMock(LoggerMock&&) = delete;
     LoggerMock& operator=(LoggerMock&&) = delete;
 
-    static LoggerMock*& Instance()
-    {
-        static LoggerMock* mock;
-        return mock;
-    }
+    static LoggerMock& Instance() { return *instance_; }
 
     MOCK_METHOD(void, OpenLog, (const std::string& folder, const std::string& fileName, bool toConsole));
     MOCK_METHOD(void, CloseLog, ());
@@ -35,27 +31,10 @@ public:
     MOCK_METHOD(void, MakeInfoLogEntry, (const std::string& str));
     MOCK_METHOD(void, MakeWarnLogEntry, (const std::string& str));
     MOCK_METHOD(void, MakeErrorLogEntry, (const std::string& str));
+
+private:
+    static LoggerMock* instance_;
 };
-
-inline void MakeDebugLogEntry(const std::string& fn, const std::string& str)
-{
-    // No need to test DebugLogEntry
-}
-
-inline void MakeInfoLogEntry(const std::string& fn, const std::string& str)
-{
-    LoggerMock::Instance()->MakeInfoLogEntry(str);
-}
-
-inline void MakeWarnLogEntry(const std::string& fn, const std::string& str)
-{
-    LoggerMock::Instance()->MakeWarnLogEntry(str);
-}
-
-inline void MakeErrorLogEntry(const std::string& fn, const std::string& str)
-{
-    LoggerMock::Instance()->MakeErrorLogEntry(str);
-}
 
 }  // namespace Tile
 
---
+++ Game/tile_test/Src/Mock/TmxLogMock.cpp
@@ -0,0 +1,37 @@
+/*
+ *	Copyright (C) 2023 Anders Wennmo
+ *	This file is part of forestadventure which is released under MIT license.
+ *	See file LICENSE for full license details.
+ */
+
+#include "Mock/TmxLogMock.h"
+
+namespace FA {
+
+namespace Tile {
+
+LoggerMock* LoggerMock::instance_;
+
+void MakeDebugLogEntry(const std::string& fn, const std::string& str)
+{
+    // No need to test DebugLogEntry
+}
+
+void MakeInfoLogEntry(const std::string& fn, const std::string& str)
+{
+    LoggerMock::Instance().MakeInfoLogEntry(str);
+}
+
+void MakeWarnLogEntry(const std::string& fn, const std::string& str)
+{
+    LoggerMock::Instance().MakeWarnLogEntry(str);
+}
+
+void MakeErrorLogEntry(const std::string& fn, const std::string& str)
+{
+    LoggerMock::Instance().MakeErrorLogEntry(str);
+}
+
+}  // namespace Tile
+
+}  // namespace FA
