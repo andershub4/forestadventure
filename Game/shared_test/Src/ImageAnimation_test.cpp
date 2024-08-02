456ce18af154a49edaa41a567ccf817d505e2643
Anders Wennmo <anders.wennmo@gmail.com>
2024-08-02 20:31:23 +0200

    Refactor ImageAnimationTest

--- Game/shared_test/Src/ImageAnimation_test.cpp
+++ Game/shared_test/Src/ImageAnimation_test.cpp
@@ -26,6 +26,21 @@ protected:
         , spriteMock_(std::make_shared<StrictMock<Graphic::SpriteMock>>())
     {}
 
+    std::shared_ptr<ImageAnimation> CreateAnimationWithEmptyContent(
+        std::shared_ptr<StrictMock<Graphic::SpriteMock>> spriteMock,
+        std::shared_ptr<StrictMock<SequenceMock<Shared::ImageFrame>>> seqMock)
+    {
+        EXPECT_CALL(*seqMock, IsEmpty).WillOnce(Return(true));
+        return std::make_shared<ImageAnimation>(spriteMock, seqMock);
+    }
+    std::shared_ptr<ImageAnimation> CreateAnimation(
+        std::shared_ptr<StrictMock<Graphic::SpriteMock>> spriteMock,
+        std::shared_ptr<StrictMock<SequenceMock<Shared::ImageFrame>>> seqMock)
+    {
+        EXPECT_CALL(*seqMock, IsEmpty).WillOnce(Return(false));
+        return std::make_shared<ImageAnimation>(spriteMock, seqMock);
+    }
+
     Graphic::TextureMock textureMock_;
     sf::IntRect rect_{0, 0, 10, 12};
     ImageFrame frame_{&textureMock_, rect_, {5, 6}};
@@ -35,64 +50,57 @@ protected:
 
 TEST_F(ImageAnimationTest, UpdateWithEmptyContentShouldDoNothing)
 {
-    EXPECT_CALL(*seqMock_, IsEmpty).WillOnce(Return(true));
-    ImageAnimation animation_(spriteMock_, seqMock_);
-    animation_.Update(0.01f);
+    auto animation = CreateAnimationWithEmptyContent(spriteMock_, seqMock_);
+    animation->Update(0.01f);
 }
 
 TEST_F(ImageAnimationTest, UpdateWithContentShoulSetTexture)
 {
-    EXPECT_CALL(*seqMock_, IsEmpty).WillOnce(Return(false));
-    ImageAnimation animation(spriteMock_, seqMock_);
+    auto animation = CreateAnimation(spriteMock_, seqMock_);
     EXPECT_CALL(*seqMock_, Update(0.01f));
     EXPECT_CALL(*seqMock_, GetCurrent).WillOnce(Return(frame_));
     EXPECT_CALL(*spriteMock_, setTextureImpl(Address(&textureMock_), false));
     EXPECT_CALL(*spriteMock_, setTextureRect(Eq(rect_)));
-    animation.Update(0.01f);
+    animation->Update(0.01f);
 }
 
 TEST_F(ImageAnimationTest, UpdateWithContentAndCenterShoulSetTextureAndSetOrigin)
 {
-    EXPECT_CALL(*seqMock_, IsEmpty).WillOnce(Return(false));
-    ImageAnimation animation(spriteMock_, seqMock_);
-    EXPECT_CALL(*seqMock_, Update(0.01f));
+    auto animation = CreateAnimation(spriteMock_, seqMock_);
+    EXPECT_CALL(*seqMock_, Update(_));
     EXPECT_CALL(*seqMock_, GetCurrent).WillOnce(Return(frame_));
-    EXPECT_CALL(*spriteMock_, setTextureImpl(Address(&textureMock_), false));
-    EXPECT_CALL(*spriteMock_, setTextureRect(Eq(rect_)));
+    EXPECT_CALL(*spriteMock_, setTextureImpl(_, _));
+    EXPECT_CALL(*spriteMock_, setTextureRect(_));
     EXPECT_CALL(*spriteMock_, setOrigin(5, 6));
-    animation.Center();
-    animation.Update(0.01f);
+    animation->Center();
+    animation->Update(0.01f);
 }
 
 TEST_F(ImageAnimationTest, IntersectsWithIntersectingSpritesShouldReturnTrue)
 {
-    EXPECT_CALL(*seqMock_, IsEmpty).WillOnce(Return(false));
-    ImageAnimation animation(spriteMock_, seqMock_);
+    auto animation = CreateAnimation(spriteMock_, seqMock_);
     auto otherSeqMock = std::make_shared<StrictMock<SequenceMock<Shared::ImageFrame>>>();
     auto otherSpriteMock = std::make_shared<StrictMock<Graphic::SpriteMock>>();
-    EXPECT_CALL(*otherSeqMock, IsEmpty).WillOnce(Return(false));
-    ImageAnimation other(otherSpriteMock, otherSeqMock);
+    auto other = CreateAnimation(otherSpriteMock, otherSeqMock);
     sf::FloatRect otherRect{0, 0, 10, 10};
     sf::FloatRect rect{2, 2, 2, 2};
     EXPECT_CALL(*spriteMock_, getGlobalBounds).WillOnce(Return(rect));
     EXPECT_CALL(*otherSpriteMock, getGlobalBounds).WillOnce(Return(otherRect));
-
-    bool result = animation.Intersects(other);
+    bool result = animation->Intersects(*other);
     EXPECT_TRUE(result);
 }
 
 TEST_F(ImageAnimationTest, RegisterUpdateCBShouldBeCalledDuringUpdate)
 {
-    EXPECT_CALL(*seqMock_, IsEmpty).WillOnce(Return(false));
-    ImageAnimation animation(spriteMock_, seqMock_);
+    auto animation = CreateAnimation(spriteMock_, seqMock_);
     MockFunction<void(const ImageAnimationIf&)> callbackFunctionMock;
-    animation.RegisterUpdateCB(callbackFunctionMock.AsStdFunction());
+    animation->RegisterUpdateCB(callbackFunctionMock.AsStdFunction());
     EXPECT_CALL(*seqMock_, Update(0.01f));
     EXPECT_CALL(*seqMock_, GetCurrent).WillOnce(Return(frame_));
-    EXPECT_CALL(*spriteMock_, setTextureImpl(Address(&textureMock_), false));
-    EXPECT_CALL(*spriteMock_, setTextureRect(Eq(rect_)));
-    EXPECT_CALL(callbackFunctionMock, Call(Ref(animation)));
-    animation.Update(0.01f);
+    EXPECT_CALL(*spriteMock_, setTextureImpl(_, _));
+    EXPECT_CALL(*spriteMock_, setTextureRect(_));
+    EXPECT_CALL(callbackFunctionMock, Call(Ref(*animation)));
+    animation->Update(0.01f);
 }
 
 }  // namespace Shared
